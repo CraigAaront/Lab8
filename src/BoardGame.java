4308298f6f063f0a062a31b9d6fc23ec6c5d747e
Aaron Craig <aaron.t.craig-1@ou.edu>
2019-03-14 13:26:28 -0500

    added constructor to gameboard

--- src/BoardGame.java
+++ src/BoardGame.java
@@ -5,5 +5,10 @@ public class BoardGame
     protected LinkedHashMap<String, GamePiece> playerPieces;
     protected LinkedHashMap<String, Location> playerLocations;
     
+    public BoardGame() {
+        playerPieces = new LinkedHashMap<String, GamePiece>();
+        playerLocations = new LinkedHashMap<String, Location>();
+    }
+    
     
 }
