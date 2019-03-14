6494ee3365155417b5051e00004283ae552e6448
Aaron Craig <aaron.t.craig-1@ou.edu>
2019-03-14 13:19:44 -0500

    finished gamepiece.java

--- src/GamePieceAppearance.java
+++ src/GamePieceAppearance.java
@@ -1,6 +1,18 @@
 
 public class GamePieceAppearance
 {
-
-
+    private Color color;
+    private Shape shape;
+    
+    public GamePieceAppearance(Color color, Shape shape) {
+        this.color = color;
+        this.shape = shape;
+    }
+    
+    public Shape getShape() {
+        return this.shape;
+    }
+    public Color getColor() {
+        return this.color;
+    }
 }
