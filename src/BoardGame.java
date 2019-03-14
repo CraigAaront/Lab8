a974a769f56057efdf6d8be8ed20995c7de53e8b
Aaron Craig <aaron.t.craig-1@ou.edu>
2019-03-14 13:25:05 -0500

    started boardgame class

--- src/BoardGame.java
+++ src/BoardGame.java
@@ -1,4 +1,9 @@
+import java.util.LinkedHashMap;
+
 public class BoardGame 
 {
+    protected LinkedHashMap<String, GamePiece> playerPieces;
+    protected LinkedHashMap<String, Location> playerLocations;
+    
     
 }
